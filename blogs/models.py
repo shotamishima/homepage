2944ad92909621e198e37401bd9838c29cabedd7
ShotaMishima <sm4mma@gmail.com>
2020-09-12 19:04:38 +0900

    add photo

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Blogs(models.Model):
+    image = models.ImageField(upload_to='images/')
+    text = models.CharField(max_length=200)
